bbcb75b2f6eda777fab8a22af7750637faae6b6d
smdeile <smdeile@gmail.com>
2020-05-03 22:03:47 +0300

    Updates

---
+++ precache-manifest.40e9cd58dc2009ac85ac6463dce7c00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b068108d0193299404af523dd7d27c53",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "95fe2707d0894923c70e",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.dfce2374.chunk.css"
+  },
+  {
+    "revision": "165d51a6c6b3a970807a",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e5dee852.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e5dee852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fe2707d0894923c70e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.d5b17410.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
